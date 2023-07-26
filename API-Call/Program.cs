8f69283db6a12df12500ad1f0fab732bf0e50b0e
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-07-26 21:46:46 +0900

    API呼び出し

---
+++ API-Call/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+Console.WriteLine("問題！ 答えが３０になる足し算の組み合わせは？");
+Console.Write("1つめ：");
+int one = int.Parse(Console.ReadLine());
+Console.Write("2つめ：");
+int two = int.Parse(Console.ReadLine());
+
+// web apiを呼ぶための httpClient作成
+var client = new HttpClient();
+
+// POSTでrequestを投げる
+var response = await client.PostAsJsonAsync($"https://localhost:7059/?answer1={one}&answer2={two}", new RequestBody { Answer = one + two });
+
+if (response.IsSuccessStatusCode)
+{
+    var responseBody = await response.Content.ReadFromJsonAsync<Response>();
+    Console.WriteLine(responseBody.Result);
+    Console.WriteLine(responseBody.Result2);
+}
+else
+{
+    Console.WriteLine("接続に失敗しました");
+}
+
+
+//リクエストとレスポンス
+class RequestBody
+{
+    public int? Answer { get; set; }
+}
+class Response
+{
+    public string? Result { get; set; }
+    public int? Result2 { get; set; }
+}
